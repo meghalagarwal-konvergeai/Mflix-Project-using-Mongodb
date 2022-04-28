cfb2bdd0598044196b2cb42b8c8202198b5d60ed
meghalagarwal-konvergeai <meghal.agarwal@konverge.ai>
2022-04-28 22:44:11 +0530

    Created Mflix Project using Mongodb

--- mflix/db.py
+++ mflix/db.py
@@ -562,7 +562,7 @@ def most_active_commenters():
     """
     # TODO: User Report
     # Return the 20 users who have commented the most on MFlix.
-    pipeline = []
+    pipeline = [] 
 
     rc = db.comments.read_concern # you may want to change this read concern!
     comments = db.comments.with_options(read_concern=rc)
